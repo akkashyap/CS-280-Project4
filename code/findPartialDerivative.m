38b4cd55ceefb5b4818b9a05b12f32e018761729
Tim Brown <tbrown126@dhcp-46-149.eecs.berkeley.edu>
2013-11-05 13:25:57 -0800

    editted findPartialDerivative to now use the gaussian derivative filter and added the graph of results.

--- code/findPartialDerivative.m
+++ code/findPartialDerivative.m
@@ -1,18 +1,19 @@
 function [ dx, dy ] = findPartialDerivative(im)
 
-%dxKernel = [-1, 0 1; -1, 0 1; -1, 0 1];
-%dyKernel = [1, 1, 1; 0,0,0; -1, -1, -1];
-
-dxKernel = [-1, 0 1]; 
+dxKernel = [-1, 0, 1]; 
 dyKernel = [-1; 0; 1];
 
-gauss = fspecial('gaussian', [3, 3], .5);
-r = conv2(double(t), double([1 , -1]), 'same')
+gauss = fspecial('gaussian', 3, 2);
 
+dx = conv2(double(im),double(conv2(dxKernel,gauss)), 'same');
 
-dx = conv2(double(im),double(dxKernel), 'same');
+dy = conv2(double(im),double(conv2(dyKernel,gauss)), 'same');
 
-dy = conv2(double(im),double(dyKernel), 'same');
-
+%% Without the use of Gaussian derivative filter
+%
+% dx = conv2(double(im),double(dxKernel), 'same');
+%
+% dy = conv2(double(im),double(dyKernel,gauss), 'same');
+%%
 end
 
